ba6ae44f301a8b3efaeacacd6ffb05461fd0df85
Joshua Marron <marronjosh@hotmail.com>
2018-07-21 00:21:23 +0100

    fiddling with styles

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import { Component } from 'preact';
 
 import { Header } from "./header";
 import { Bio } from "./bio";
+import { Cv } from "./cv";
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <Header />
-                <Bio />
+                <div class="body-box">
+                    <Bio />
+                    <Cv />
+                </div>
             </div>
         );
     }
