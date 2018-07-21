b57137db9071c410cda8df8fa5db3bf55620b366
Joshua Marron <marronjosh@hotmail.com>
2018-07-21 20:04:48 +0100

    Added styling for bio section

--- src/bio.js
+++ src/bio.js
@@ -1,9 +1,15 @@
+import "../css/bio";
+import bioPic from "../assets/webpic.png";
+
 export function Bio() {
     return (
         <div class="bio-div">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut luctus at neque in tristique. Etiam tincidunt, felis in posuere pretium, nisl ligula varius velit, sed malesuada tellus odio eget augue. Praesent dictum sagittis enim, et malesuada dui tincidunt eu. Cras non massa non elit imperdiet interdum eu ac nunc. Fusce rutrum orci eget velit luctus vulputate. Sed lorem sem, dignissim nec sapien non, rhoncus varius nunc. Fusce quis egestas quam. Sed turpis leo, ullamcorper vitae ante egestas, ultrices elementum justo. Fusce mollis pellentesque dapibus. Donec gravida dolor mauris, quis laoreet quam fringilla tempor. Vestibulum sed lorem vitae risus sodales egestas. Suspendisse semper, urna sit amet sollicitudin tincidunt, metus eros imperdiet dui, sed ultricies dui ante et velit. Donec aliquet iaculis ante, eu luctus ipsum sollicitudin ut. Pellentesque congue blandit mauris, vitae molestie quam malesuada vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et placerat ante. 
+            <p class="bio-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut luctus at neque in tristique. Etiam tincidunt, felis in posuere pretium, nisl ligula varius velit, sed malesuada tellus odio eget augue. Praesent dictum sagittis enim, et malesuada dui tincidunt eu. Cras non massa non elit imperdiet interdum eu ac nunc. Fusce rutrum orci eget velit luctus vulputate. Sed lorem sem, dignissim nec sapien non, rhoncus varius nunc. Fusce quis egestas quam. Sed turpis leo, ullamcorper vitae ante egestas, ultrices elementum justo. Fusce mollis pellentesque dapibus. Donec gravida dolor mauris, quis laoreet quam fringilla tempor. Vestibulum sed lorem vitae risus sodales egestas. Suspendisse semper, urna sit amet sollicitudin tincidunt, metus eros imperdiet dui, sed ultricies dui ante et velit. Donec aliquet iaculis ante, eu luctus ipsum sollicitudin ut. Pellentesque congue blandit mauris, vitae molestie quam malesuada vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et placerat ante.
             </p>
+            <div class="bio-pic-box">
+                <img class="bio-pic" src={bioPic} alt="Joshua Marron" />
+            </div>
         </div>
     );
 }
